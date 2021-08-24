f37b36b2a9e9d5cfe0f76ee7ae8a0eab30654fa6
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-08-24 09:16:09 +0200

    add scafolding for controller/downloader

---
+++ download/download/controller.py
@@ -0,0 +1,15 @@
+"""
+Download data from external data sources, given a url, into the local data directory.
+"""
+
+
+class Controller:
+
+    def __init__(self, frequency):
+        
+        self.frequency = frequency
+
+    def start(self):
+        pass
+
+        #TODO: loop for calling search and download using the frequency.

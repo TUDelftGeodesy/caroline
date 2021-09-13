f6af1b1640a01002f2cc54cddc1cb1ba099d57f1
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-09-13 12:27:13 +0200

    implement use  of data class in ASF api

--- download/download/search.py
+++ download/download/search.py
@@ -8,7 +8,7 @@ from shapely.geometry import Polygon
 import datetime
 import os
 import hashlib
-
+from . import data_product
 
 class DataSearch(ABC):
     '''
@@ -120,7 +120,7 @@ class SciHub(DataSearch):
     def search(self, aoi, start_date, end_date=None, track=None, polarisation=None, orbit_direction=None, 
         sensor_mode='IW', product='SLC', instrument_name='Sentinel-1'):
         '''
-        Build query using the input search creteria and request the result to the API.
+        Searches the SciHub API for datasets based on input creteria.
 
         Args:
             aoi (str): a polygon geometry formatted as well-known-text (A.K.A.: area of interest)
@@ -134,18 +134,12 @@ class SciHub(DataSearch):
             product (str): SciHub product level as in the SciHub documentation. E.g., SLC
             instrument (str): name of the platform as in the SciHub documentation. E.g., Sentinel-1
 
-        Returns: a listing found products. 
+        Returns: a list of products. 
         '''
 
         # TODO: Define what metadata should be collected for each products/image
 
-        self.products = [] # collect title, id, and uri to download
-        self.footprints = []
-        self.tracks = []
-        self.orbit_directions = []
-        self.ids = []
-        self.dates = []
-        self.polarisations = []
+        self.products = [] # collection of products
 
         print("Searching for products....")
         query = self.build_query(aoi, start_date, end_date, track, polarisation, orbit_direction, sensor_mode, product, instrument_name)
@@ -157,6 +151,7 @@ class SciHub(DataSearch):
         print("Found ", result_json['feed']["opensearch:totalResults"], " products.")
         if len(entries) !=0:
             for entry in entries:
+
                 product = {'title': entry['title'], 'id': entry['id'], 'uri': entry['link'][0]['href']}
                 self.products.append(product)
 
@@ -165,7 +160,7 @@ class SciHub(DataSearch):
 
         return self.products
         
-        #TODO: requirement time shall be propvided in different formats sucha as a single specific time, a list of specific times, an interval, or  list of intervals
+        #TODO: requirement time shall be propvided in different formats such as a single specific time, a list of specific times, an interval, or  list of intervals
         # though for the system only two formats might be necessary. A single time (considering the temporal resolution of the sensor), and an interval with a start and end
 
 
@@ -248,6 +243,8 @@ class SciHub(DataSearch):
 
 
 
+
+
 class ASF(DataSearch):
     '''
     Implementation of SearchAPI for the Alaska Satellite Facility
@@ -286,7 +283,7 @@ class ASF(DataSearch):
             product (str): processing level as in the ASF documentation. E.g., SLC
             instrument (str): name of the platform as in the ASF documentation. E.g., Sentinel-1
 
-        Returns: query string 
+        Returns: query string with JSON output type 
 
         '''
 
@@ -313,7 +310,7 @@ class ASF(DataSearch):
         
         query = ''
         if instrument_name:
-            # using a leading '&' means parameters can be input in any order
+            # using a leading '&' means parameters can be omitted at will
             query += '&platform=' + instrument_name
         if sensor_mode:
             query += '&beamMode=' + sensor_mode
@@ -337,17 +334,126 @@ class ASF(DataSearch):
         print(search_url+ query)
         return search_url + query
             
+    def search(self, aoi, start_date, end_date=None, track=None, polarisation=None, orbit_direction=None, 
+    sensor_mode='IW', product='SLC', instrument_name='Sentinel-1') -> None:
+        '''
+        Searches the ASF API for datasets based on input creteria.
         
-    def search(self, *argv):
-        pass
+        Args:
+            aoi (str): a polygon geometry formatted as well-known-text (A.K.A.: area of interest)
+            start_date (str): first day for search (YYYY-MM-DD)
+            end_date (str): last day for search as (YYYY-MM-DD), If None, the start_date will be also used as end_date
+            and the query will use a time-window of one day.
+            track (int): the number of the for the searching creteria
+            polarisation (str): type of polarisation. A single value or a comma-separated list of values E.g., VV or VV,HH
+            orbit_direction (srt): Direction of the orbit. E.g., Ascending, Descending 
+            sensor_mode (str): beam mode as in the ASF documentation. E.g., IW
+            product (str): processing level as in the ASF documentation. E.g., SLC
+            instrument (str): name of the platform as in the ASF documentation. E.g., Sentinel-1
+
+        Returns: a list of products.
+
+        '''
+
+        print("Searching for products....")
+        query = self.build_query(aoi, start_date, end_date, track, polarisation, orbit_direction, sensor_mode, product, instrument_name)
+
+        search_results = self.connector.get(query)
+        result_json = search_results.json() # returns array of objects
+        print(result_json)
+        _objects = result_json[0]
+
+        print("Found ", str(len(_objects)), " products.")
+
+        if len(_objects) != 0:
+            for _object in _objects:
+                product = data_product.Product(_object['productName'], _object['sceneId'], _object['downloadUrl'], checksum=_object['md5sum'])
+                self.products.append(product)
+        else:
+            print("No products found for this creteria")
+        
+        return self.products
 
 
+    def download(self, products, download_directory, max_retries=3):
+        '''
+        Downloads dataset given for a list of products.
 
-    def download(self, *argv):
-        pass
+        Args:
+            products (obj): list of products as defined by the Product dataclass.
+            directory: path to directory to store files.
+            max_reties (int): maximum number of connection retries to download a product.
 
-    def validate_download(self, *argv):
-        pass
+        '''
+
+        if os.path.exists(download_directory) == False:
+            os.mkdir(download_directory)
+        
+        print("Downloading Products....")
+
+        for product in products:
+            file_path = download_directory + product['title'] + '.zip'
+
+            # Avoid re-download valid products after sudden failure
+            if os.path.isfile(file_path) and self.validate_download(product, file_path):
+                continue
+            
+            validity = False
+            download_retries = 1 # counter
+
+            while validity == False:
+                if download_retries > max_retries:
+                    print("Download failed after", str(max_retries), "tries. Product: ", product.title)
+                    break
+                else:
+                    response = self.connector.get(product.uri, stream=True)
+                    with open(file_path, 'wb' ) as f:
+                        # f.write(b'file content')
+                        for chunk in response.iter_content(chunk_size=100*1024): # bytes
+                            f.write(chunk)
+                
+                    print('>>>> Trying', str(download_retries) )
+                    download_retries += 1
+                    validity = self.validate_download(product, file_path)
+                
+        return None
+
+
+    #TODO: needs to adapt to fetch the checksum
+    def validate_download(self, product, file_path):
+        """
+        Validates the success of a dowload by performing a data integrity check using checksums.
+
+        Args:
+            product (dic): product description including a URI for data download
+            file_path (str): path to local copy of the product.
+        
+        Return: 
+            validity chek (bolean)
+
+        """
+
+        # extract checksum on remote (MD5)
+        remote_checksum = product['checksum']
+
+        print('remote checksum:', remote_checksum )
+
+        # Local checksum
+        with open (file_path, 'rb') as local_file:
+            file_hash = hashlib.md5()
+            while chunk := local_file.read(100*128): # chunk size must be multiple of 128 bytes
+                file_hash.update(chunk)
+        
+        
+        local_checksum = file_hash.hexdigest()
+        print('local chechsum:', local_checksum)
+
+        if remote_checksum == local_checksum:
+            result = True
+        else:
+            result = False
+
+        return result 
 
 
 class EarthData(DataSearch):

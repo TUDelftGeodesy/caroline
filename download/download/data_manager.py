a7505d1ddcb529228a344a79c2399c28ebbb923f
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-09-20 14:53:44 +0200

    creates dataManager class

---
+++ download/download/data_manager.py
@@ -0,0 +1,10 @@
+"""
+Manage the storage location of products downloaded by the download engine
+"""
+
+class DataManager:
+    """ Tacks the location (file path) of downloaded products from any API"""
+
+    def __init__(self) -> None:
+        pass
+    

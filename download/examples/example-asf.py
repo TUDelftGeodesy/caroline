75339af228b4c516e51aae1e8e204c2c4cba3484
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-22 15:20:27 +0100

    move examples directory

--- download/examples/example-asf.py
+++ download/examples/example-asf.py
@@ -6,8 +6,6 @@ This example is for the ASF API
 Must provide your own account credentials
 """
 
-
-
 from download import connector
 from download.asf import ASF
 
@@ -25,7 +23,7 @@ def products_to_dict(product_list):
 # from download import connector
 # from download.asf import ASF
 
-# WARNING: This example will download 4 datasets (>16GB)
+# WARNING: This example will download 1 datasets (>4GB)
 
 # Create a connector to handle the autentification
 
@@ -39,15 +37,24 @@ search_api = ASF(c)
 # search the API 
 
 search_results=search_api.search('POLYGON((-155.75 18.90,-155.75 20.2,-154.75 19.50,-155.75 18.90))',
-        '2018-04-22', '2018-05-08', orbit_direction='Ascending',
+        '2018-04-22', '2018-05-01', orbit_direction='Ascending',
         sensor_mode='IW', product='SLC', instrument_name='Sentinel-1')
 
+
+# TODO: check if data will be accessible in the HPC
+
+# Freek:
+# Slide on polygons and  data conversions, kml, shape, WKT, 
+# mention modularity of code
+# Mention about SPIDER, plannig to do so:
+## 
+
 results = products_to_dict(search_results)
-print(results)
-print(type(results))
+# print(results)
+# print(type(results))
 
-print(results.values())
-
+# print(results.values())
 
 # Download datasets (a.k.a products found by search())
-# search_api.download(search_results, './data/') # This might take a long time
+
+search_api.download(search_results) # This might take a long time

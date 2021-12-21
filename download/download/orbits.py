afa1175f6877b8dfc8c6f06f4a15f61842194323
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-12-21 12:40:33 +0100

    clean code

--- download/download/orbits.py
+++ download/download/orbits.py
@@ -23,26 +23,6 @@ parser_conf = subparsers.add_parser("conf", help="download satellite datasets us
 parser_conf.add_argument("start", help="start date for the search.", type=str)
 parser_conf.add_argument("end", help="end date for the search.", type=str)
 # Options
-# geometry_group = parser_conf.add_mutually_exclusive_group()
-# geometry_group.add_argument("-a", "--aoi", help="area of interest as WKT (enclose in double-quotes if necessary)", type=str)
-# geometry_group.add_argument("-f", "--file",
-#                     help="file descrbing an area of interest. SHP or KML",
-#                     type=str)
-
-# parser_conf.add_argument("-o", "--orbit",
-#                     help="flight direction along the orbit. 'Ascending' or 'Descending'. Default: 'Ascending'", 
-#                     default="Ascending",
-#                     type=str)
-# parser_conf.add_argument("-m", "--mode",
-#                     help="sensor mode. Default: 'IW'", 
-#                     default="IW",
-#                     type=str)
-                
-# parser_conf.add_argument("-p", "--prod",
-#                     help="product's processing level. Default: 'SLC'", 
-#                     default="SLC",
-#                     type=str)
-
 parser_conf.add_argument("-t", "--retry",
                     help="maximun number of retries on download failures. Default: 3", 
                     default=3,
@@ -56,58 +36,13 @@ parser_manual.add_argument("start", help="start date for the search.", type=str)
 parser_manual.add_argument("end", help="end date for the search.", type=str)
 
 # Options
-# geometry_group = parser_manual.add_mutually_exclusive_group()
-# geometry_group.add_argument("-a", "--aoi", help="area of interest as WKT (enclose in double-quotes if necessary)", type=str)
-# geometry_group.add_argument("-f", "--file",
-#                     help="file descrbing an area of interest. SHP or KML",
-#                     type=str)
-
-# parser_manual.add_argument("-o", "--orbit",
-#                     help="flight direction along the orbit. 'Ascending' or 'Descending'. Default: 'Ascending'", 
-#                     default="Ascending",
-#                     type=str)
-# parser_manual.add_argument("-m", "--mode",
-#                     help="sensor mode. Default: 'IW'", 
-#                     default="IW",
-#                     type=str)
-                
-# parser_manual.add_argument("-p", "--prod",
-#                     help="product's processing level. Default: 'SLC'", 
-#                     default="SLC",
-#                     type=str)
-
 parser_manual.add_argument("-t", "--retry",
                     help="maximun number of retries on download failures. Default: 3", 
                     default=3,
                     type=int)
 
 
-
-
-
-
 args = parser.parse_args()
-print(args)
-
-
-# # -f or --file option
-# if args.file is not None:
-#     extension = pathlib.Path(args.file).suffix
-#     if extension == ".shp":
-#         geo_ = utils.read_shapefile(args.file)
-#         if len(geo_) == 1:
-#             args.aoi = geo_[0].wkt  
-#         else:
-#             RuntimeError("The file must contain a single geometry")
-#     elif extension == ".kml":
-#         geo_ = utils.read_kml(args.file)
-#         if len(geo_) == 1:
-#             args.aoi = geo_[0].wkt  
-#         else:
-#             RuntimeError("The file must contain a single geometry")
-#     else:
-#         raise TypeError("File extension not supported. Use '.shp' or '.kml' ")
-
 
 #Create connector
 if USERNAME is not None and PASSWORD is not None:
@@ -118,7 +53,6 @@ else:
 c.test_connection()
 
 # instantiate API 
-
 search_api = S1OrbitProvider(c)
 
 search_results=search_api.search(args.start, end_date=args.end)

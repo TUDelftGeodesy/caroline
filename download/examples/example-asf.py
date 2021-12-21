ba91cff9be95db42e8bc2ece92ff0d40f0116ea1
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-12-21 09:25:42 +0100

    update asf example file

--- download/examples/example-asf.py
+++ download/examples/example-asf.py
@@ -6,7 +6,8 @@ This example is for the ASF API
 Must provide your own account credentials
 """
 
-from download import connector
+from ..download  import connector
+# from download import connector
 from download.asf import ASF
 
 def products_to_dict(product_list):
@@ -18,15 +19,9 @@ def products_to_dict(product_list):
 
     return dictonary
 
-
-# for DAG
-# from download import connector
-# from download.asf import ASF
-
 # WARNING: This example will download 1 datasets (>4GB)
 
 # Create a connector to handle the autentification
-
 c = connector.Connector("<username>", "<password>", 'https://api.daac.asf.alaska.edu/', retain_auth=True)
 
 c.test_connection()
@@ -35,23 +30,9 @@ c.test_connection()
 search_api = ASF(c)
 
 # search the API 
-
-
 search_results=search_api.search('POLYGON((-155.75 18.90,-155.75 20.2,-154.75 19.50,-155.75 18.90))',
         '2018-04-22', '2018-05-01', orbit_direction='Ascending',
         sensor_mode='IW', product='SLC', instrument_name='Sentinel-1')
 
-
-# TODO: check if data will be accessible in the HPC
-
-# Freek:
-# Slide on polygons and  data conversions, kml, shape, WKT, 
-# mention modularity of code
-# Mention about SPIDER, plannig to do so:
-## 
-
-
-
 # Download datasets (a.k.a products found by search())
-
 search_api.download(search_results) # This might take a long time

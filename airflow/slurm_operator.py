8beccb3f159093d51c278fc277c5b4869fac9656
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-03-15 12:20:16 +0100

    working version of slurm operator

--- airflow/slurm_operator.py
+++ airflow/slurm_operator.py
@@ -12,50 +12,57 @@ from airflow.configuration import conf
 from base64 import b64encode
 
 class SlurmOperator(SSHOperator):
-    def __init__(self, sbatch_command: str, sleep_time="1m", **kwargs) -> None:
+    def __init__(self, sbatch_command: str, monitor_time="1m", output_file=None, **kwargs) -> None:
         """Submits a job using sbacth to Spider. The job statatus is monitored until completed or failed.
            Inherits properties and methods from the SSHOperator.
            Requires an SSHHook.
         Args:
-            sbatch_command (str): command to start a slurm job using the sbatch command. E.g., sbatch <path to script.sh>
-            sleep_time: time interval at which the status of a job will be checked. Defaults 1 minute.
+            sbatch_command (str): command to submit a slurm script. E.g., sbatch <path to script.sh>
+            monitor_time: time interval at which the status of a job will be checked. Default is 1 minute.
+            output_file: path to directory for the slurm output file. If None, output file will be in
+                         home directory.
             """
     
         super().__init__(**kwargs) # inherit properties from parent class
         self.command = sbatch_command 
-        self.sleep_time  = sleep_time
+        self.monitor_time  = monitor_time
+        self.output_file = output_file
 
     def execute(self, context=None) -> Union[bytes, str]:
         result: Union[bytes, str]
         if self.command is None:
             raise AirflowException("SSH operator error: SSH command not specified. Aborting.")
 
+        # Set default directory for slurm output files
+        if self.output_file is None:
+            self.output_file = "~/"
+
         # Forcing get_pty to True if the command begins with "sudo".
         self.get_pty = self.command.startswith('sudo') or self.get_pty
         
-        prefix ="""
-            cd /project/caroline/Software/slurm &&
-            JID=$(""" + self.command + """)
-            echo  $JID
-            sleep 10s 
-            """ 
-    
-        loop = """
-            ST="PENDING"
-            while [ "$ST" != "COMPLETED" ] ; do 
-                ST=$(sacct -j ${JID##* } -o State | awk 'FNR == 3 {print $1}')
-                sleep """ + self.sleep_time + \
-                + """
+        submit ="""
+        cd """ + self.output_file + """
+        JID=$(""" + self.command + """)
+        echo  $JID
+        sleep 10s 
+        """
 
-                if [ "$ST" == "FAILED" ]; then
-                    echo 'Job final status:' $ST, exiting...
-                    exit 122
-                fi; done
-                
-            echo $ST
-            """ 
-            
-        command = prefix + loop
+        monitor = """
+        ST="PENDING"
+        while [ "$ST" != "COMPLETED" ]
+        do
+            ST=$(sacct -j ${JID##* } -o State | awk 'FNR == 3 {print $1}')
+            sleep """ + self.monitor_time + """
+            if [ "$ST" == "FAILED" ]
+            then
+                echo 'Job final status:' $ST, exiting...
+                exit 122
+            fi
+        done
+        echo $ST
+        """
+
+        command = submit + monitor
 
         try:
             with self.get_ssh_client() as ssh_client:

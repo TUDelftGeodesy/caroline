23809150219c3726d8838a39921c4916fdd14097
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-03-15 13:52:03 +0100

    organize dags

--- airflow/dags/benelux.py
+++ airflow/dags/benelux.py
@@ -1,6 +1,6 @@
-# A DAG for the creation of inteferograms on Spider 
+# A DAG for the creation of inteferograms for the Benelux area.
 # PARAMETERS
-    # dag_run.conf["start_date"]
+    # dag_run.conf["start_date"] 
     # dag_run.conf["end_date"]
     # dag_run.conf["geometry"]
 # USAGE: 
@@ -11,9 +11,6 @@
 # airflow dags trigger 'template' -r 'run-002' --conf '{"start_date":"2021-12-19", "end_date":"2021-12-22", "geometry":"POLYGON((-155.75 18.90,-155.75 20.2,-154.75 19.50,-155.75 18.90))"}'
 
 # TODO: implement the case for the Benelux area.
-https://marclamberti.com/blog/airflow-sensors/
-https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/sensors/filesystem/index.html
-
 
 from datetime import timedelta, datetime
 
@@ -52,7 +49,7 @@ default_args = {
 
 
 with DAG(
-    dag_id='template_2',
+    dag_id='template',
     default_args=default_args,
     description='Test DAG download',
     schedule_interval=timedelta(days=1),
@@ -63,36 +60,28 @@ with DAG(
     # Bash commands
     cmd_download_radar ="""
     source /project/caroline/Software/bin/init.sh &&
-    source /project/caroline/Software/download/python-gdal/bin/activate &&
+    source /project/caroline/Software/download/venv39/bin/activate &&
     cd /project/caroline/Software/caroline/download/download/ &&
-    module load python/3.9.6  gdal/3.4.1 &&
+    module load python/3.9.6 &&
     python engine.py conf '{{dag_run.conf["start_date"]}}' '{{dag_run.conf["end_date"]}}' -a '{{dag_run.conf["geometry"]}}'
     """
 
     cmd_download_orbits ="""
     source /project/caroline/Software/bin/init.sh &&
-    source /project/caroline/Software/download/python-gdal/bin/activate &&
+    source /project/caroline/Software/download/venv39/bin/activate &&
     cd /project/caroline/Software/caroline/download/download/ &&
-    module load python/3.9.6 gdal/3.4.1 &&
+    module load python/3.9.6 &&
     python orbits.py conf '{{dag_run.conf["start_date"]}}' '{{dag_run.conf["end_date"]}}'
     """
 
     cmd_processing="""
     source /project/caroline/Software/bin/init.sh &&
-    source /project/caroline/Software/download/python-gdal/bin/activate &&
+    source /project/caroline/Software/download/venv39/bin/activate &&
     cd /project/caroline/Software/caroline/download/download/ &&
-    module load python/3.9.6 gdal/3.4.1 &&
+    module load python/3.9.6 
     # call DORIS-RIPPL
     """
 
-    # what comes next:
-    # A. send notification by email
-    # B. write an operator with for slurm with pyslurm?
-    # c. consult sensors/operator creation with saverio, invite Niels.
-
-    # eScience project
-
-    
     download_radar = SSHOperator(
     task_id='download_radar',
     command=cmd_download_radar,

3c3451f93322474c2a6ee779f6606809f099631f
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-03-31 22:42:19 +0200

    Revert "implement nargs for resolution and polarisation values"
    This reverts commit 118093b58f2d822ef71f1232c817cdefe675b337.

--- processing/processing/interferogram.py
+++ processing/processing/interferogram.py
@@ -40,10 +40,9 @@ if __name__ == '__main__':
                     default="SLC",
                     type=str)
     parser.add_argument("-pl", "--pol",
-                    help="Polarizations of Sentinel-1 Data. A list of strings. E.g. 'VV' or 'VV' 'HV'", 
-                    nargs='+',
+                    help="Polarizations of Sentinel-1 Data. A string with a single polarization value or a list of strings. E.g. 'VV' or ['VV', 'HV'] ", 
                     default='VV',
-                    type=str)
+                    type=list)
     parser.add_argument("-tk", "--track",
                     help="Processing track number", 
                     default= 37,
@@ -55,8 +54,7 @@ if __name__ == '__main__':
                     type=str)
 
     parser.add_argument("-R", "--resolution",
-                    help="Pixel resolutions for the output datasets. A list of values (integers). E.g., 500 or 500 1000 2000 ", 
-                    nargs='+',
+                    help="Pixel resolutions for the output datasets. A single value or a list of values (integers). E.g., 500, or [500, 1000] ", 
                     type=int)
     
     parser.add_argument("-md", "--master_date",
@@ -65,51 +63,50 @@ if __name__ == '__main__':
                     type=str)
 
     args = parser.parse_args()
-    print(args.pol)
-    print(args.resolution)
 
     # =====================================================================
     # Check validity of processing boundary when using -f or --file option
     # =====================================================================
-    # TODO: [CAR-44] Modify check file extension of AoI to avoid FileExistsError
-    # if args.file is not None:
-    #     extension = pathlib.Path(args.file).suffix
-    #     if extension == ".shp":
-    #         geo_ = utils.read_shapefile(args.file)
-    #         if len(geo_) == 1:
-    #             args.aoi = geo_[0].wkt  
-    #         else:
-    #             RuntimeError("The file must contain a single geometry")
-    #     elif extension == ".kml":
-    #         geo_ = utils.read_kml(args.file)
-    #         if len(geo_) == 1:
-    #             args.aoi = geo_[0].wkt  
-    #         else:
-    #             RuntimeError("The file must contain a single geometry")
-    #     else:
-    #         raise TypeError("File extension not supported. Must be '.shp' or '.kml' ")
+    if args.file is not None:
+        extension = pathlib.Path(args.file).suffix
+        if extension == ".shp":
+            geo_ = utils.read_shapefile(args.file)
+            if len(geo_) == 1:
+                args.aoi = geo_[0].wkt  
+            else:
+                RuntimeError("The file must contain a single geometry")
+        elif extension == ".kml":
+            geo_ = utils.read_kml(args.file)
+            if len(geo_) == 1:
+                args.aoi = geo_[0].wkt  
+            else:
+                RuntimeError("The file must contain a single geometry")
+        else:
+            raise TypeError("File extension not supported. Must be '.shp' or '.kml' ")
 
     # =====================================================================
     # Check validity of list arguments: 
     # =====================================================================
 
-    # TODO: [CAR-45] Review validity checks for resolution and polatization values
-    if list_of_data_type(args.pol, data_type=str) is False:
-        raise TypeError (f"Value for --pol argument must be a list of strings. Is {args.pol}")
+    if isinstance(args.pol, list):
+        if list_of_data_type(args.pol, data_type=str) is False:
+            raise TypeError (f"Value for --pol argument must be a list of strings. Is {args.pol}")
     else:
-        polarisation = args.pol
+        polarisation = [args.pol] # internally values for this argument will always be treated as a list
 
-    if list_of_data_type(args.resolution, data_type=int) is False:
-        raise TypeError (f"Value for --resolution argument must be a list of integers. Is {args.resolution}")
-    else: 
-        pixel_resolution = [args.resolution] # internally values for this argument will always be treated as a list
+    if isinstance(args.resolution, list):
+        if list_of_data_type(args.resolution, data_type=int) is False:
+            raise TypeError (f"Value for --resolution argument must be a list of integers. Is {args.resolution}")
+
+    else: pixel_resolution = [args.resolution] # internally values for this argument will always be treated as a list
+
      
     processing_boundary = ReadWriteShapes()  # takes SHP, KML, or WKT
     if args.aoi is None:
         # This expects the file to be in the doris-rippl data directory
         processing_boundary(args.file)
     else:
-        processing_boundary(args.aoi) # For Rippl this must be a list of coordinates.
+        processing_boundary(args.aoi)
      
     study_area_shape = processing_boundary.shape.buffer(args.buffer)
 
--- processing/tests/test_interferogram.py
+++ processing/tests/test_interferogram.py
@@ -2,13 +2,11 @@
 
 import unittest
 import os
-import processing.interferogram
 
 PATH = '../processing/interferogram.py'
 SHAPEFILE = "test-data/test.shp"
 KML_FILE = "test-data/test.kml"
 WKT = "POLYGON((-155.75 18.90,-155.75 20.2,-154.75 19.50,-155.75 18.90))"
-POL = ['VV']
 
 class TestArgumentAoi(unittest.TestCase):
     """ Test the validity of inputs for the argument Area of Interest (aoi)"""
@@ -19,22 +17,6 @@ class TestArgumentAoi(unittest.TestCase):
         stream = os.popen('echo my command')
         output = stream.read()
         #TODO: complete test
-
-
-class TestArgumentPolarization(unittest.TestCase):
-    """Test the values for this argument are valid. A list of strings"""
-
-    def test_is_list(self):
-        """ test values are a list"""
-        
-        pass
-
-    def test_is_string():
-        """Test values in the last are strings"""
-
-        pass
-
-
 
 if __name__ == '___main___':
     unittest.main()

4ac794ae826ceba4f36fd9c659f7442ece3ec941
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-03-31 16:24:17 +0200

    delete list_of_datatype function

--- processing/processing/interferogram.py
+++ processing/processing/interferogram.py
@@ -9,24 +9,7 @@ import numpy as np
 from download import utils
 from rippl.processing_templates.general_sentinel_1 import GeneralPipelines
 from rippl.orbit_geometry.read_write_shapes import ReadWriteShapes
-
-def list_of_data_type(_input:list, data_type=str) -> bool:
-    """Test if input is a list of items of type 'data_type'
-    Args:
-        _input (list): list to be tested
-        data_type: the python datatype that items in the list must be
-    Returns true if inputs is a list of strings
-    """
-    if len(_input) <= 0:
-        raise TypeError("Empty list")
-
-    result = True
-    for item in _input:
-        test = isinstance(item, data_type)
-        result = result and test
-
-    return result
-
+from utils import list_of_data_type
 
 if __name__ == '__main__':
     # First we read the input start and end date for the processing

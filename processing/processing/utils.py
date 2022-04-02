98c8a3b7268a2e98c156359691b2689815c2a877
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-04-02 19:46:37 +0200

    add function to conver WKT to rippl coordinate pairs

--- processing/processing/utils.py
+++ processing/processing/utils.py
@@ -2,6 +2,9 @@
 A library of functions used accross modules
 """
 
+from turtle import st
+import shapely.wkt
+
 def list_of_data_type(_input:list, data_type=str) -> bool:
     """Check if input is a list of items of type 'data_type'
     Args:
@@ -21,3 +24,22 @@ def list_of_data_type(_input:list, data_type=str) -> bool:
         result = result and test
 
     return result
+
+def wkt_to_list(wkt:str) -> list:
+    """Converts well-know-text polygon to Doris-Rippl list of coordinates polygon representation
+    Args:
+        wkt (str):  polygon as well know text.
+    Returns:
+        A nested list of coordinate pairs, such as [[x1,y1], [x2,y2]] 
+    """
+
+    polygon = shapely.wkt.loads(wkt)
+    wkt_coords = polygon.exterior.coords
+    coords_list = []
+    
+    for coord in wkt_coords:
+        # srtip coordinate pairs (tupples) and construct list of lists
+        coords_list.append( [coord[0], coord[1]] )
+
+    return coords_list
+

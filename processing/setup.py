131eed4220851e9a51589138ac769a3f157d5b6a
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-04-02 19:48:17 +0200

    create setup.py for processing package

--- processing/setup.py
+++ processing/setup.py
@@ -1,21 +1,21 @@
-# standard library
-import dotenv
-from setuptools import setup, find_packages
-
 # local library
 import processing
+# standard library
+from setuptools import find_packages, setup
 
-# setup
-install_requires = ['python-dotenv', 'doris-rippl']
+
 
 setup(
     name="processing",
-    version=processing.__version__,
+    version="0.0.2",
     author="Caroline Developer Team, TU Delft",
     author_email="",
     packages=find_packages(include=['processing', 'processing.*']),
-    install_requires=install_requires,
+    install_requires=   ['python-dotenv', 
+                        'shapely', 
+                        'gdal'
+                        ],
     description="Data processing component for the Caroline System",
     classifiers=["Programming Language :: Python :: 3",
                  "Operating System :: OS Independent", ],
-    python_requires='>=3.8',)
+    python_requires='>=3.8')

50096e35ebca623f70bf89c6db862682f660f0d6
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-04-04 15:38:28 +0200

    add clean up function when script exits

--- processing/processing/inteferogram.sh
+++ processing/processing/inteferogram.sh
@@ -8,13 +8,23 @@
 #SBATCH -p normal		# partition (queue); job can run up to 5 days
 #SBATCH --qos=long		
 
-#--- Run your application ---
 echo "Start"; date
 TEMP_DIR="/project/caroline/Share/users/caroline-mgarcia/tmp"
+
+# Cleanup temporary folder
+function clean_up {
+  rm --recursive --force "$TEMP_DIR" && echo "Clean up of $tmp_dir completed successfully."
+  exit
+}
+
+# Setup clean_up to run on exit
+trap 'clean_up' EXIT
+
+#--- Run your application ---
 
 source /project/caroline/Software/caroline/caroline-venv/bin/activate
 cd /project/caroline/Share/users/caroline-mgarcia
 
-python interferogram.py -s 20160101 -e 20160120 -c 5 -n test_stack -f amsterdam.kml -R 2000 -t $TEMP_DIR -md 20160107 
+python interferogram.py -s 20160101 -e 20160120 -c 5 -n test_stack -f amsterdam.kml -R 2000 -pl VV -t $TEMP_DIR -md 20160107 
 
 echo "Done"; date

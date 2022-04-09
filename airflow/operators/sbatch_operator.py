52c68ce6a653210a150ef879485b1698da57ab71
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-04-09 23:31:35 +0200

    fix issue with expaning bash variable

--- airflow/operators/sbatch_operator.py
+++ airflow/operators/sbatch_operator.py
@@ -11,55 +11,17 @@ from airflow.configuration import conf
 from base64 import b64encode
 
 
-def create_sbatch_script(script_file: str, commands:str, max_time:str, cores=1, tasks=1,  nodes=1, partition='normal', qos='long')-> None:
-    """
-    Creates a bash script describing a Slurm job for Spider.
-
-    Args:
-        script_file: path and name for the sbash script with .sh extension.
-        commands: bash commands to be for the body of the sbash script.
-        cores: number of cores to request to the cluster.
-        tasks: number of tasks to request to the cluster.
-        max_time: maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
-        nodes: number of node to request to the cluster.
-        partition: partition type.
-        qos: quality of service to request to the cluster.
-    """
-    header = f"""
-    #!/bin/bash\n
-    #SBATCH -N {nodes}			# number of nodes
-    #SBATCH -c {cores}			# number of cores; coupled to 8000 MB memory per core
-    #SBATCH -t {max_time}		# maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
-    #SBATCH -p {partition}		# partition (queue); job can run up to 5 days
-    #SBATCH --qos={qos}		
-    #SBATCH --ntasks={tasks}
-    """
-
-    body = f"""
-    echo "Starting time:"; date
-    {commands}
-    echo "Finished at:"; date
-    """
-
-    # return  header + body
-    with open(f'{script_file}', 'w') as script_file:
-        script_file.write(header)
-        script_file.write(body)
-    
-    return None
-
-
 class SBATCHOperator(SSHOperator):
-    def __init__(self, sbatch_commands: str, script_file: str, max_time:str, frequency="1m", job_output=None,  cores=1, tasks=1,  nodes=1, partition='normal', qos='long', **kwargs) -> None:
+    def __init__(self, sbatch_commands: str, script_name: str, max_time:str, frequency="1m", output_dir=None,  cores=1, tasks=1,  nodes=1, partition='normal', qos='long', **kwargs) -> None:
         """Submits a job using an sbacth script to Spider. The job status is monitored until complete or failure.
            Inherits properties and methods from the SSHOperator.
            Requires an SSHHook.
         Args:
             sbatch_command (str): commands for the body of the sbatch script.
-            script_file (str): path and name for the sbash script.
+            script_name (str): name for the sbash script. A '.sh' file
             max_time (str): maximum run time for the slurm job, [HH:MM:SS] or [MM:SS] or [minutes]
             frequency (str): time interval at which the status of a job will be checked. Default is 1 minute.
-            job_output (str): path to directory for the slurm output file. If None, output file will be in
+            job_output (str): path to directory for the  sbatch script and theslurm output file. If None, output file will be in
                 home directory.
             cores: number of cores to request to the cluster.
             tasks: number of tasks to request to the cluster.
@@ -71,8 +33,8 @@ class SBATCHOperator(SSHOperator):
         super().__init__(**kwargs) # inherit properties from parent class
         self.commands = sbatch_commands 
         self.monitoring_frequency  = frequency
-        self.job_output = job_output
-        self.script_file = script_file
+        self.output_dir = output_dir
+        self.script_name = script_name
         self.max_time = max_time
         self.cores = cores
         self.tasks = tasks
@@ -86,26 +48,28 @@ class SBATCHOperator(SSHOperator):
             raise AirflowException("SBATCH operator error: command for the body of script not specified. Aborting.")
 
         # Set default directory for slurm output files
-        if self.job_output is None:
-            self.job_output = "~/"
+        if self.output_dir is None:
+            self.output_dir = "~/"
 
         # Forcing get_pty to True if the command begins with "sudo".
         self.get_pty = self.commands.startswith('sudo') or self.get_pty
 
         # prepare sbatch script
-        create_sbatch_script(script_file=self.script_file, 
-                            commands=self.commands, 
-                            max_time=self.max_time, 
-                            cores=self.cores, 
-                            tasks=self.tasks, 
-                            nodes=self.nodes, 
-                            partition=self.partition, 
-                            qos=self.qos
+        sbatch_body = self.create_sbatch_body( 
+                            self.commands, 
+                            self.max_time, 
+                            self.cores, 
+                            self.tasks, 
+                            self.nodes, 
+                            self.partition, 
+                            self.qos
                             )
 
+        # Use single qoutes around <sbatch_body> with printf to avoid expanding variables
         submit_job= f"""
-        cd {self.job_output}
-        JID=$(sbatch {self.script_file})
+        cd {self.output_dir}
+        printf '{sbatch_body}' > {self.script_name}
+        JID=$(sbatch {self.script_name})
         echo  $JID
         sleep 10s 
         """
@@ -124,6 +88,7 @@ class SBATCHOperator(SSHOperator):
         done
         echo $ST
         """
+
         command = submit_job + monitoring
 
         try:
@@ -136,3 +101,38 @@ class SBATCHOperator(SSHOperator):
         if not enable_pickling:
             result = b64encode(result).decode('utf-8')
         return result
+    
+    def create_sbatch_body(self, commands:str, max_time:str, cores=1, tasks=1,  nodes=1, partition='normal', qos='long')-> str:
+        """
+        Creates a bash script describing a Slurm job for Spider.
+
+        Args:
+            script_file: path and name for the sbash script with .sh extension.
+            commands: bash commands to be for the body of the sbash script.
+            cores: number of cores to request to the cluster.
+            tasks: number of tasks to request to the cluster.
+            max_time: maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
+            nodes: number of node to request to the cluster.
+            partition: partition type.
+            qos: quality of service to request to the cluster.
+        """
+
+        header = f"""#!/bin/bash
+        #SBATCH -N {nodes}			# number of nodes
+        #SBATCH -c {cores}			# number of cores; coupled to 8000 MB memory per core
+        #SBATCH -t {max_time}		# maximum run time in [HH:MM:SS] or [MM:SS] or [minutes]
+        #SBATCH -p {partition}		# partition (queue); job can run up to 5 days
+        #SBATCH --qos={qos}		
+        #SBATCH --ntasks={tasks}
+        """
+
+        body = f"""
+        echo "Starting time:"; date
+        {commands}
+        echo "Finished at:"; date
+        """
+    
+        return header + body
+
+if __name__ == '__main__':
+    pass

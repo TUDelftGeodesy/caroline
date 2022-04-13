f634f5edeca4b5544b043d4abc1ec577f4d060bd
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-04-13 13:24:09 +0200

    add type of orbit as optional argument

--- download/download/orbits.py
+++ download/download/orbits.py
@@ -20,26 +20,35 @@ PASSWORD = os.getenv('ORB_PASSWORD')
 ASF_BASE_URL = os.getenv('ORB_BASE_URL')
 
 # Positional Arguments
-parser = argparse.ArgumentParser(prog="Caroline Download Engine for Orbits", description="Search and downloads orbit datasets from the SciHub facility")
+parser = argparse.ArgumentParser(prog="Download Orbit files", description="Search and downloads orbit files for Sentinel 1.")
 subparsers = parser.add_subparsers(help="Subcommands. Options for 'man' and 'conf' the same and -a is always required.")
 # conf command
 parser_conf = subparsers.add_parser("conf", help="download satellite datasets using a configuration file, an .env file")
 parser_conf.add_argument("start", help="start date for the search.", type=str)
 parser_conf.add_argument("end", help="end date for the search.", type=str)
+
 # Options
+parser_conf.add_argument("ot", "--type", 
+                    help="orbit type. E.g., RES = restituted, POE = precise. Default: RES ", 
+                    default='RES', 
+                    type=str)
 parser_conf.add_argument("-t", "--retry",
                     help="maximun number of retries on download failures. Default: 3", 
                     default=3,
                     type=int)
 
 # manual command
-parser_manual = subparsers.add_parser("man", help="manual mode. Start download using username and password")
+parser_manual = subparsers.add_parser("man", help="manual mode. Start download by providing username and password")
 parser_manual.add_argument("user", help="ASF DAAC account's username", type=str)
 parser_manual.add_argument("password", help="ASF DAAC account's password", type=str)
 parser_manual.add_argument("start", help="start date for the search.", type=str)
 parser_manual.add_argument("end", help="end date for the search.", type=str)
 
 # Options
+parser_manual.add_argument("ot", "--type", 
+                    help="orbit type. E.g., RES = restituted, POE = precise. Default: RES ", 
+                    default='RES', 
+                    type=str)
 parser_manual.add_argument("-t", "--retry",
                     help="maximun number of retries on download failures. Default: 3", 
                     default=3,
@@ -65,7 +74,7 @@ search_api = S1OrbitProvider(c)
 start_date = utils.convert_to_dashed_date(args.start)
 end_date = utils.convert_to_dashed_date(args.end)
 
-search_results=search_api.search(start_date=start_date, end_date=end_date)
+search_results=search_api.search(start_date=start_date, end_date=end_date, osvtype=args.type)
 
 search_api.download(search_results, max_retries=args.retry)
 

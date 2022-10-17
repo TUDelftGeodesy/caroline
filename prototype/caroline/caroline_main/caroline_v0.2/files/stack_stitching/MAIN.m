26fde34d3b0e35588fa997185ebaadf4ecc91cb6
Philip Conroy <p.n.conroy@tudelft.nl>
2022-10-17 16:49:05 +0200

    deleted redundant MAIN.m, deleted stamps_prep.m, removed stack id from output filenames

--- prototype/caroline/caroline_main/caroline_v0.2/files/stack_stitching/MAIN.m
+++ prototype/caroline/caroline_main/caroline_v0.2/files/stack_stitching/MAIN.m
@@ -4,9 +4,6 @@
 % name of new folder
 folder = '{AoI_name}_cropped_stack';
 
-% id is used in filenaming
-id = '{AoI_name}';
-
 addpath(genpath('{caroline_dir}/caroline_v{version}/files/stack_stitching/floris'));
 
 % croptype = boundingbox: specify bounding box (for example lon_bb = [4.22;4.66], lat_bb = [51.89;52.02])
@@ -15,7 +12,7 @@ croptype = 'poly'; % boundingbox, full or poly
 cropparam = '{shape_dir}/{AoI_name}_shape.shp';    
 
 %% stitch images
-stitch_S1_stack(folder,id,croptype,cropparam);
+stitch_S1_stack(folder,croptype,cropparam);
 
 %% make slcs
 create_slcs(folder,id);

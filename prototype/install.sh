3253a4e728d4c2173ecb94a6f7e7726ab317616e
Niels Jansen <N.H.Jansen@tudelft.nl>
2023-01-27 16:55:19 +0100

    Add installation script for caroline prototype

---
+++ prototype/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# install-caroline-prototype.sh
+
+CAROLINE_PROTOTYPE_DIR='/project/caroline/Software/caroline-prototype'
+
+if [ -d "$CAROLINE_PROTOTYPE_DIR" ]; then
+	BACKUP_DIR="${CAROLINE_PROTOTYPE_DIR}-$(date +%Y%m%dT%H%M%S)"
+	echo "Creating backup of exisiting Caroline prototype directory"
+	if [ -d "${BACKUP_DIR}" ]; then
+		echo 'ERROR: Backup directory already exists. This should not happen' >&2
+		echo 'Aborting. >&2'
+		exit 1
+	fi
+	mv -v "${CAROLINE_PROTOTYPE_DIR}" "${BACKUP_DIR}"
+fi
+
+if [ ! -d "${CAROLINE_PROTOTYPE_DIR}" ]; then
+	echo "Creating Caroline prototype directory"
+	mkdir -pv "${CAROLINE_PROTOTYPE_DIR}"
+fi
+
+for DIR in files main run_files; do
+	cp -Rvp caroline/caroline_main/caroline_v0.2/${DIR}/ "${CAROLINE_PROTOTYPE_DIR}"
+done
+
+# Eof

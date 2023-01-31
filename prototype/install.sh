815e426b446f535d9923406be6d3022f423fa658
Niels Jansen <N.H.Jansen@tudelft.nl>
2023-01-31 13:07:32 +0100

    Rename Caroline home dir

--- prototype/install.sh
+++ prototype/install.sh
@@ -2,24 +2,24 @@
 #
 # install-caroline-prototype.sh
 
-CAROLINE_PROTOTYPE_DIR='/project/caroline/Software/caroline-prototype'
+CAROLINE_HOME='/project/caroline/Software/caroline-prototype'
 
-if [ -d "$CAROLINE_PROTOTYPE_DIR" ]; then
-	BACKUP_DIR="${CAROLINE_PROTOTYPE_DIR}-$(date +%Y%m%dT%H%M%S)"
+if [ -d "$CAROLINE_HOME" ]; then
+	BACKUP_DIR="${CAROLINE_HOME}-$(date +%Y%m%dT%H%M%S)"
 	echo "Creating backup of exisiting Caroline prototype directory"
 	if [ -d "${BACKUP_DIR}" ]; then
 		echo 'ERROR: Backup directory already exists. This should not happen' >&2
 		echo 'Aborting. >&2'
 		exit 1
 	fi
-	mv -v "${CAROLINE_PROTOTYPE_DIR}" "${BACKUP_DIR}"
+	mv -v "${CAROLINE_HOME}" "${BACKUP_DIR}"
 fi
 
-if [ ! -d "${CAROLINE_PROTOTYPE_DIR}" ]; then
+if [ ! -d "${CAROLINE_HOME}" ]; then
 	echo "Creating Caroline prototype directory"
-	mkdir -pv "${CAROLINE_PROTOTYPE_DIR}"
+	mkdir -pv "${CAROLINE_HOME}"
 fi
 
-cp -Rvp caroline/caroline_main/caroline_v0.2/ "${CAROLINE_PROTOTYPE_DIR}"
+cp -Rvp caroline/caroline_main/caroline_v0.2/ "${CAROLINE_HOME}"
 
 # Eof

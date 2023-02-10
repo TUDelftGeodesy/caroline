2eccde2f6d20d836cf7108982d450870a5d0a7f3
Niels Jansen <N.H.Jansen@tudelft.nl>
2023-02-10 15:41:53 +0100

    Use proper paths to files

--- prototype/caroline/caroline_main/bin/run-caroline.sh
+++ prototype/caroline/caroline_main/bin/run-caroline.sh
@@ -15,21 +15,20 @@ echo "\$CAROLINE: $CAROLINE"
 echo "\$CAROLINE_BIN: $CAROLINE_BIN"
 echo "\$CAROLINE_WORK: $CAROLINE_WORK"
 echo "\$PATH: $PATH"
-exit
-
-RUN_TS=$(date +%Y%m%dT%H%M%S)
 
 # Run script to find if any new files have been downloaded since we last checked and
 # save the list of newly downloaded files in an output file
-find-new-insar-files.sh > "new-insar-files-${RUN_TS}.out"
+RUN_TS=$(date +%Y%m%dT%H%M%S)
+NEW_INSAR_FILES_FILE="${CAROLINE_WORK}/new-insar-files-${RUN_TS}.out"
+find-new-insar-files.sh > "${NEW_INSAR_FILES_FILE}"
 
 # If the output file with downloaded files has more than 0 bytes, that means new files
 # have been downloaded
-if [ -s "new-insar-files-${RUN_TS}.out" ]; then
+if [ -s "${NEW_INSAR_FILES_FILE}" ]; then
 	# Check if the downloaded files pertain to tracks we are interested in
 	TRACKS=$(cat new-insar-files-${RUN_TS}.out \
 		| cut -d/ -f7 \
 		| sort -u \
-		| grep -F -f $CAROLINE_HOME/area-track-lists/veenwijden.dat)
+		| grep -F -f ${CAROLINE}/area-track-lists/veenwijden.dat)
 	echo $TRACKS
 fi

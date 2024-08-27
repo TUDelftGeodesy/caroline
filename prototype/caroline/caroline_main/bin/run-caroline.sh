6cebe18d8a681e433b74c324f3f071e0128a3854
Simon van Diepen <s.vandiepen98@gmail.com>
2024-08-27 15:50:00 +0200

    Allow for multiple AoIs within Caroline

--- prototype/caroline/caroline_main/bin/run-caroline.sh
+++ prototype/caroline/caroline_main/bin/run-caroline.sh
@@ -22,37 +22,44 @@ RUN_TS=$(date +%Y%m%dT%H%M%S)
 NEW_INSAR_FILES_FILE="${CAROLINE_WORK}/new-insar-files-${RUN_TS}.out"
 find-new-insar-files.sh > "${NEW_INSAR_FILES_FILE}"
 
-# If the output file with downloaded files has more than 0 bytes, that means new files
-# have been downloaded
-if [ -s "${NEW_INSAR_FILES_FILE}" ]; then
-	# Check if the downloaded files pertain to tracks we are interested in
-	TRACKS=$(cat "${NEW_INSAR_FILES_FILE}" \
-		| cut -d/ -f7 \
-		| sort -u \
-		| grep -F -f ${CAROLINE}/area-track-lists/nl_amsterdam.dat)
-fi
+# If the output file with downloaded files has more than 32 bytes, that means new files
+# have been downloaded (the No new files found message is 32 bytes, new files found is 500+ bytes)
+if [ "$(cat ${NEW_INSAR_FILES_FILE} | wc -c)" -gt "32" ]; then
 
-# If we found new files for tracks we are interested in
-if [ ! -z "${TRACKS}" ]; then
+  # Loop over the available area-track-lists & corresponding parameter files in run-files
+  # area-track-lists ATL.dat requires parameter file param_file_Caroline_v1_0_spider_ATL.txt
+  for AREA in `ls ${CAROLINE}/area-track-lists/`
+  do
+    # Check if the downloaded files pertain to tracks we are interested in
+    TRACKS=$(cat "${NEW_INSAR_FILES_FILE}" \
+      | cut -d/ -f7 \
+      | sort -u \
+      | grep -F -f ${CAROLINE}/area-track-lists/${AREA})
 
-	# Convert tracks list into csv
-	TRACKS_CSV=$(echo ${TRACKS} | tr ' ' ',')
+    # If we found new files for tracks we are interested in
+    if [ ! -z "${TRACKS}" ]; then
 
-	# Submit caroline core to job queue
-	#
-	# Load required python and gdal modules
-  source /etc/profile.d/modules.sh
-	source /project/caroline/Software/bin/init.sh
-	module load python/3.9.6 gdal/3.4.1
-	#
-	# Load required python environment with gdal
-	source /project/caroline/Share/users/caroline-svandiepen/virtual_envs/caroline_v2/bin/activate
-	#
-	# Chdir to script directory
-	cd ${CAROLINE}/caroline_v1.0/run_files/
-	#
-	# Submit the job to the cluster's scheduler (slurm)
-	./Caroline_v1_0.sh \
-		--config-file param_file_Caroline_v1_0_spider_nl_amsterdam.txt \
-		--tracks "${TRACKS_CSV}"
+      # Convert tracks list into csv
+      TRACKS_CSV=$(echo ${TRACKS} | tr ' ' ',')
+
+      # Submit caroline core to job queue
+      #
+      # Load required python and gdal modules
+      source /etc/profile.d/modules.sh
+      source /project/caroline/Software/bin/init.sh
+      module load python/3.9.6 gdal/3.4.1
+      #
+      # Load required python environment with gdal
+      source /project/caroline/Share/users/caroline-svandiepen/virtual_envs/caroline_v2/bin/activate
+      #
+      # Chdir to script directory
+      cd ${CAROLINE}/caroline_v1.0/run_files/
+      #
+      # Submit the job to the cluster's scheduler (slurm) with the correct parameter file determined by the AREA name
+      sbatch ./Caroline_v1_0.sh \
+        --config-file param_file_Caroline_v1_0_spider_$(echo ${AREA} | cut -d. -f1).txt \
+        --tracks "${TRACKS_CSV}"
+    fi
+  done
+
 fi

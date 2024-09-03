ef2308776824a52920053f1749db485f6752208d
Simon van Diepen <s.vandiepen98@gmail.com>
2024-09-03 14:23:07 +0200

    Added filter for empty directories

--- prototype/caroline/caroline_main/caroline_v1.0/bin/utils/filter_bad_images.py
+++ prototype/caroline/caroline_main/caroline_v1.0/bin/utils/filter_bad_images.py
@@ -1,5 +1,6 @@
 from sys import argv
 from read_param_file import read_param_file
+import glob
 filename, param_file, cpath, AoI_name = argv
 
 search_parameters = ['doris_directory', 'track', 'asc_dsc']
@@ -14,6 +15,7 @@ for track in range(len(tracks)):
     data = f.read().split("\n")
     f.close()
 
+    # check for incomplete downloads
     bad_zips = []
     for line in data:
         if line == "":
@@ -29,6 +31,17 @@ for track in range(len(tracks)):
             bad_zip = dirr.split("/")[0]
             if bad_zip not in bad_zips:
                 bad_zips.append(bad_zip)
+
+    # check for directories without zip files
+    dirs = glob.glob("{}/{}_s1_{}_t{:0>3d}/good_images/2*".format(out_parameters['doris_directory'], AoI_name,
+                                                                  asc_dsc[track], tracks[track]))
+    for dr in dirs:
+        files = glob.glob(f'{dr}/*.zip')
+        if len(files) == 0: # no zip files present
+            bad_zip = dr.split('/')[-1]
+            if bad_zip not in bad_zips:
+                bad_zips.append(bad_zip)
+
     f = open("{}/{}_s1_{}_t{:0>3d}/good_images/bad_zips.txt".format(out_parameters['doris_directory'], AoI_name,
                                                                     asc_dsc[track], tracks[track]), "w")
     for zipp in bad_zips:

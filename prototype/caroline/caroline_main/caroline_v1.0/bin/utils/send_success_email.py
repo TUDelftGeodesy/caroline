ca6894b11f4ff2ad6b7ac9be9efe99a1c3c779ff
Simon van Diepen <s.vandiepen98@gmail.com>
2024-10-10 10:40:32 +0200

    Add output locations to email

--- prototype/caroline/caroline_main/caroline_v1.0/bin/utils/send_success_email.py
+++ prototype/caroline/caroline_main/caroline_v1.0/bin/utils/send_success_email.py
@@ -2,7 +2,8 @@ from sys import argv
 from read_param_file import read_param_file
 filename, param_file, cpath = argv
 
-search_parameters = ['track', 'asc_dsc', 'do_coregistration', 'do_stack_stitching', 'do_depsi', 'do_depsi_post', 'sensor']
+search_parameters = ['track', 'asc_dsc', 'do_coregistration', 'do_stack_stitching', 'do_depsi', 'do_depsi_post', 'sensor',
+                     'coregistration_directory', 'stitch_directory', 'depsi_directory']
 out_parameters = read_param_file(cpath, param_file, search_parameters)
 
 tracks = eval(out_parameters['track'])
@@ -18,7 +19,8 @@ if len(tracks_formatted) == 1:
 run_id = param_file.split('_spider_')[1].split('/')[0][:-16]
 
 
-def print_mail(run_id, track, sensor, dv5, stitch, depsi, dppu, portal_link):
+def print_mail(run_id, track, sensor, dv5, stitch, depsi, dppu, portal_link, coreg_dir, stitch_dir, depsi_dir,
+               depsipost_dir):
     print("""Dear radargroup,
 
 A new CAROLINE run has just finished on run {run_id}! 
@@ -28,10 +30,10 @@ Track(s): {tracks}
 Sensor: {sensor}
 
 The following steps were run:
-Coregistration: {dv5}
-Stitching: {stitch}
-Depsi: {depsi}
-Depsi-post & portal upload: {dppu}
+Coregistration: {dv5} {coreg_dir}
+Stitching: {stitch} {stitch_dir}
+Depsi: {depsi} {depsi_dir}
+Depsi-post & portal upload: {dppu} {depsipost_dir}
 
 {portal_link}
 
@@ -46,7 +48,11 @@ Freek, Niels, and Simon""".format(tracks=track,
                                   depsi=depsi,
                                   dppu=dppu,
                                   run_id=run_id,
-                                  portal_link=portal_link))
+                                  portal_link=portal_link,
+                                  coreg_dir=coreg_dir,
+                                  stitch_dir=stitch_dir,
+                                  depsi_dir=depsi_dir,
+                                  depsipost_dir=depsipost_dir))
 
 
 print_mail(run_id=run_id,
@@ -56,4 +62,8 @@ print_mail(run_id=run_id,
            stitch="Yes" if eval(out_parameters['do_stack_stitching']) == 1 else "No",
            depsi="Yes" if eval(out_parameters['do_depsi']) == 1 else "No",
            dppu="Yes" if eval(out_parameters['do_depsi_post']) == 1 else "No",
-           portal_link="All CAROLINE results can be accessed at https://caroline.portal-tud.skygeo.com/portal/caroline/ ." if eval(out_parameters['do_depsi_post']) == 1 else "")
+           portal_link="All CAROLINE DePSI-post results can be accessed at https://caroline.portal-tud.skygeo.com/portal/caroline/ ." if eval(out_parameters['do_depsi_post']) == 1 else "",
+           coreg_dir=f"(located in {out_parameters['coregistration_directory']} )" if eval(out_parameters['do_coregistration']) == 1 else "",
+           stitch_dir=f"(located in {out_parameters['stitch_directory']} )" if eval(out_parameters['do_stack_stitching']) == 1 else "",
+           depsi_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(out_parameters['do_depsi']) == 1 else "",
+           depsipost_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(out_parameters['do_depsi_post']) == 1 else "")

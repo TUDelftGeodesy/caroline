9dab649f427d06405c75f6aa0e9e919c0aa4abb3
Simon van Diepen <s.vandiepen98@gmail.com>
2024-11-11 10:20:30 +0100

    Add parameter file to email, update DePSI nl_grijpskerk

--- prototype/caroline/caroline_main/caroline_v1.0/bin/utils/send_success_email.py
+++ prototype/caroline/caroline_main/caroline_v1.0/bin/utils/send_success_email.py
@@ -1,8 +1,10 @@
 from sys import argv
 from read_param_file import read_param_file
+
 filename, param_file, cpath = argv
 
-search_parameters = ['track', 'asc_dsc', 'do_coregistration', 'do_stack_stitching', 'do_depsi', 'do_depsi_post', 'sensor',
+search_parameters = ['track', 'asc_dsc', 'do_coregistration', 'do_stack_stitching', 'do_depsi', 'do_depsi_post',
+                     'sensor',
                      'coregistration_directory', 'stitch_directory', 'depsi_directory', 'do_reslc',
                      'reslc_directory', 'skygeo_viewer']
 out_parameters = read_param_file(cpath, param_file, search_parameters)
@@ -19,9 +21,13 @@ if len(tracks_formatted) == 1:
 
 run_id = param_file.split('_spider_')[1].split('/')[0][:-16]
 
+f = open(f'{cpath}/{param_file}')
+paramfile = f.read()
+f.close()
+
 
 def print_mail(run_id, track, sensor, dv5, stitch, depsi, dppu, portal_link, coreg_dir, stitch_dir, depsi_dir,
-               depsipost_dir, reslc, reslcdir):
+               depsipost_dir, reslc, reslcdir, paramfile, param_file):
     print("""Dear radargroup,
 
 A new CAROLINE run has just finished on run {run_id}! 
@@ -45,20 +51,27 @@ In case of questions, please contact Niels at n.h.jansen@tudelft.nl or Simon at 
 
 Kind regards,
 The CAROLINE development team,
-Freek, Niels, and Simon""".format(tracks=track,
-                                  sensor=sensor,
-                                  dv5=dv5,
-                                  stitch=stitch,
-                                  depsi=depsi,
-                                  dppu=dppu,
-                                  run_id=run_id,
-                                  portal_link=portal_link,
-                                  coreg_dir=coreg_dir,
-                                  stitch_dir=stitch_dir,
-                                  depsi_dir=depsi_dir,
-                                  depsipost_dir=depsipost_dir,
-                                  reslc=reslc,
-                                  reslc_dir=reslcdir))
+Freek, Niels, and Simon
+
+=======================================
+DEBUG info:
+--- PARAMETER FILE: {param_file} ---
+{paramfile}""".format(tracks=track,
+                      sensor=sensor,
+                      dv5=dv5,
+                      stitch=stitch,
+                      depsi=depsi,
+                      dppu=dppu,
+                      run_id=run_id,
+                      portal_link=portal_link,
+                      coreg_dir=coreg_dir,
+                      stitch_dir=stitch_dir,
+                      depsi_dir=depsi_dir,
+                      depsipost_dir=depsipost_dir,
+                      reslc=reslc,
+                      reslc_dir=reslcdir,
+                      paramfile=paramfile,
+                      param_file=param_file))
 
 
 print_mail(run_id=run_id,
@@ -68,10 +81,18 @@ print_mail(run_id=run_id,
            stitch="Yes" if eval(out_parameters['do_stack_stitching']) == 1 else "No",
            depsi="Yes" if eval(out_parameters['do_depsi']) == 1 else "No",
            dppu="Yes" if eval(out_parameters['do_depsi_post']) == 1 else "No",
-           portal_link=f"The DePSI-post results can be accessed at https://caroline.portal-tud.skygeo.com/portal/caroline/{out_parameters['skygeo_viewer']} ." if eval(out_parameters['do_depsi_post']) == 1 else "",
-           coreg_dir=f"(located in {out_parameters['coregistration_directory']} )" if eval(out_parameters['do_coregistration']) == 1 else "",
-           stitch_dir=f"(located in {out_parameters['stitch_directory']} )" if eval(out_parameters['do_stack_stitching']) == 1 else "",
-           depsi_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(out_parameters['do_depsi']) == 1 else "",
-           depsipost_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(out_parameters['do_depsi_post']) == 1 else "",
+           portal_link=f"The DePSI-post results can be accessed at https://caroline.portal-tud.skygeo.com/portal/caroline/{out_parameters['skygeo_viewer']} ." if eval(
+               out_parameters['do_depsi_post']) == 1 else "",
+           coreg_dir=f"(located in {out_parameters['coregistration_directory']} )" if eval(
+               out_parameters['do_coregistration']) == 1 else "",
+           stitch_dir=f"(located in {out_parameters['stitch_directory']} )" if eval(
+               out_parameters['do_stack_stitching']) == 1 else "",
+           depsi_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(
+               out_parameters['do_depsi']) == 1 else "",
+           depsipost_dir=f"(located in {out_parameters['depsi_directory']} )" if eval(
+               out_parameters['do_depsi_post']) == 1 else "",
            reslc="Yes" if eval(out_parameters['do_reslc']) == 1 else "No",
-           reslcdir=f"(located in {out_parameters['reslc_directory']} )" if eval(out_parameters['do_reslc']) == 1 else "")
+           reslcdir=f"(located in {out_parameters['reslc_directory']} )" if eval(
+               out_parameters['do_reslc']) == 1 else "",
+           paramfile=paramfile,
+           param_file=f"{cpath}*/{param_file}")

d19f437051101114befaebe73355c517ba56dcf3
Simon van Diepen <s.vandiepen98@gmail.com>
2024-12-03 11:03:00 +0100

    Update job summary function to work properly with squeue output

--- prototype/caroline/caroline_main/bin/summarize-running-jobs.sh
+++ prototype/caroline/caroline_main/bin/summarize-running-jobs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for JOB in `squeue --me`
+SQUEUE=`squeue --me`
+
+while [ $(echo $SQUEUE | wc -c) -gt 1 ]
 do
+  SQUEUE=$(echo ${SQUEUE} | cut -d" " -f9-)
+  JOB_ID=$(echo ${SQUEUE} | cut -d" " -f1)
+  JOB=$(echo ${SQUEUE} | cut -d" " -f-8)
   echo "
 ${JOB}
-$(grep $(echo ${JOB} | cut -d" " -f1 | xargs echo) /project/caroline/Software/caroline-prototype*/work/submitted_jobs.log)
+$(grep ${JOB_ID} /project/caroline/Software/caroline-prototype*/work/submitted_jobs.log)
 "
 done

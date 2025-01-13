8b61bc393745c9f06bb012b6e9e8c87471d3bdfd
Simon van Diepen <s.vandiepen98@gmail.com>
2025-01-13 13:36:42 +0100

    Enable continuous processing

--- prototype/caroline/caroline_main/bin/run-caroline.sh
+++ prototype/caroline/caroline_main/bin/run-caroline.sh
@@ -31,8 +31,8 @@ cd ${CAROLINE}/caroline_v1.0/run_files/
 # save the list of newly downloaded files in an output file
 RUN_TS=$(date +%Y%m%dT%H%M%S)
 NEW_INSAR_FILES_FILE="${CAROLINE_WORK}/new-insar-files-${RUN_TS}.out"
-# find-new-insar-files.sh > "${NEW_INSAR_FILES_FILE}"  ##### EDITED FOR TESTING
-echo "" > "${NEW_INSAR_FILES_FILE}"
+find-new-insar-files.sh > "${NEW_INSAR_FILES_FILE}"
+# echo "" > "${NEW_INSAR_FILES_FILE}"
 
 if [ "$(cat ${CAROLINE_WORK}/force-start-runs.dat | wc -c)" -gt "0" ]; then
   for LINE in `cat ${CAROLINE_WORK}/force-start-runs.dat`

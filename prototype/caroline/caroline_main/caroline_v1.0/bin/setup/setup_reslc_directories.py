0469b8b7063e9dc98c9f7d6de8f529981ab2b524
Simon van Diepen <s.vandiepen98@gmail.com>
2025-03-05 15:32:50 +0100

    Take sensor into account

--- prototype/caroline/caroline_main/caroline_v1.0/bin/setup/setup_reslc_directories.py
+++ prototype/caroline/caroline_main/caroline_v1.0/bin/setup/setup_reslc_directories.py
@@ -5,7 +5,7 @@ path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from read_param_file import read_param_file
 filename, param_file, cpath, AoI_name = argv
 
-search_parameters = ['coregistration_directory', 'track', 'asc_dsc', 'reslc_directory']
+search_parameters = ['coregistration_directory', 'track', 'asc_dsc', 'reslc_directory', 'sensor']
 out_parameters = read_param_file(cpath, param_file, search_parameters)
 
 tracks = eval(out_parameters['track'])
@@ -13,7 +13,8 @@ asc_dsc = eval(out_parameters['asc_dsc'])
 
 for track in range(len(tracks)):	
     try:
-        mkdir("{}/{}_s1_{}_t{:0>3d}".format(out_parameters['reslc_directory'], AoI_name, asc_dsc[track],
+        mkdir("{}/{}_{}_{}_t{:0>3d}".format(out_parameters['reslc_directory'], AoI_name,
+                                            out_parameters['sensor'].lower(), asc_dsc[track],
                                             tracks[track]))
     except OSError:
         pass  # Directory already exists

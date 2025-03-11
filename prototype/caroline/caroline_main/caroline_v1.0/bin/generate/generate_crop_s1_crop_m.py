c48fda161ed5220e49631f8d9d3b9d9bff63965b
Simon van Diepen <s.vandiepen98@gmail.com>
2025-03-11 11:21:35 +0100

    Other sensor support in crop

--- prototype/caroline/caroline_main/caroline_v1.0/bin/generate/generate_crop_s1_crop_m.py
+++ prototype/caroline/caroline_main/caroline_v1.0/bin/generate/generate_crop_s1_crop_m.py
@@ -4,7 +4,7 @@ path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from read_param_file import read_param_file
 filename, param_file, cpath, AoI_name, shape_AoI_name, version, caroline_dir = argv
 
-search_parameters = ['crop_directory', 'track', 'asc_dsc', 'shape_directory']
+search_parameters = ['crop_directory', 'track', 'asc_dsc', 'shape_directory', 'sensor']
 out_parameters = read_param_file(cpath, param_file, search_parameters)
 
 base_stack = open("{}/caroline_v{}/files/s1_crop/s1_crop.m".format(caroline_dir, version))
@@ -18,7 +18,7 @@ for track in range(len(tracks)):
 
     main = stack.format(shape_dir=out_parameters['shape_directory'],
                         caroline_dir=caroline_dir, shape_AoI_name=shape_AoI_name,
-                        version=version)
+                        version=version, sensor=out_parameters['sensor'])
 
     fw = open("{}/{}_s1_{}_t{:0>3d}/s1_crop.m".format(out_parameters['crop_directory'], AoI_name, asc_dsc[track],
                                                       tracks[track]), 'w')
--- prototype/caroline/caroline_main/caroline_v1.0/files/s1_crop/s1_crop.m
+++ prototype/caroline/caroline_main/caroline_v1.0/files/s1_crop/s1_crop.m
@@ -1,8 +1,10 @@
 %% RUN THIS IN A DIRECTORY WITH ALL CONTENTS OF THE DORIS DIRECTORY YOU AIM TO CROP SOFT LINKED
-% Crops a DORIS 5.0 stack to the bounding box of a shapefile
+% Crops a DORIS 4.0 or 5.0 stack to the bounding box of a shapefile
 % Adapted from and inspired by the stitch toolbox of F. Heuff, 2018
 %
 % By Simon van Diepen, 02/2025
+%
+% Support for other sensors by Simon van Diepen - 10/03/2025
 
 % Link code
 addpath(genpath('{caroline_dir}/caroline_v{version}/files/s1_crop'));
@@ -14,7 +16,10 @@ folder = 'cropped_stack';
 % full path to crop shapefile
 crop_file = '{shape_dir}/{shape_AoI_name}_shape.shp';
 
+% which sensor was used
+sensor = '{sensor}';
+
 %%% END OF USER SETTINGS %%%
 
-do_s1_crop(folder,crop_file);
+do_s1_crop(folder,crop_file,sensor);
 

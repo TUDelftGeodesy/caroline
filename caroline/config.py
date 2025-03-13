c0f50d2a581c65ab6a7b26cf6eabb80c2b6d35d0
Simon van Diepen <s.vandiepen98@gmail.com>
2025-03-13 11:08:39 +0100

    Run ruff over caroline

--- caroline/config.py
+++ caroline/config.py
@@ -1,9 +1,11 @@
 import os
 
+
 def config_db():
+    """Return the environment variables for the database."""
     return {
-        'host': os.environ.get('CAROLINE_DB_HOST'),
-        'database': os.environ.get('CAROLINE_DB_NAME'),
-        'user': os.environ.get('CAROLINE_DB_USER'),
-        'password': os.environ.get('CAROLINE_DB_PASSWORD')
-        }
+        "host": os.environ.get("CAROLINE_DB_HOST"),
+        "database": os.environ.get("CAROLINE_DB_NAME"),
+        "user": os.environ.get("CAROLINE_DB_USER"),
+        "password": os.environ.get("CAROLINE_DB_PASSWORD"),
+    }

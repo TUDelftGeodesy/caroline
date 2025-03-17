403ea3bfd46a777bc38b3f243bf98426fb3f297a
Simon van Diepen <s.vandiepen98@gmail.com>
2025-03-17 10:33:36 +0100

    Migrate setup_crop_directories

---
+++ caroline/prepare.py
@@ -0,0 +1,55 @@
+import os
+
+from caroline.io import read_parameter_file
+from caroline.utils import format_process_folder
+
+
+def prepare_crop(parameter_file: str) -> None:
+    """Set up the directories for cropping.
+
+    Parameters
+    ----------
+    parameter_file: str
+        Absolute path to the parameter file.
+    """
+    search_parameters = [
+        "coregistration_directory",
+        "coregistration_AoI_name",
+        "track",
+        "asc_dsc",
+        "crop_directory",
+        "crop_AoI_name",
+        "sensor",
+    ]
+    out_parameters = read_parameter_file(parameter_file, search_parameters)
+
+    tracks = eval(out_parameters["track"])
+    asc_dsc = eval(out_parameters["asc_dsc"])
+
+    for track in range(len(tracks)):
+        crop_directory = format_process_folder(
+            base_folder=out_parameters["crop_directory"],
+            AoI_name=out_parameters["crop_AoI_name"],
+            sensor=out_parameters["sensor"],
+            asc_dsc=asc_dsc[track],
+            track=tracks[track],
+        )
+        coregistration_directory = format_process_folder(
+            base_folder=out_parameters["coregistration_directory"],
+            AoI_name=out_parameters["coregistration_AoI_name"],
+            sensor=out_parameters["sensor"],
+            asc_dsc=asc_dsc[track],
+            track=tracks[track],
+        )
+
+        os.makedirs(crop_directory, exist_ok=True)
+
+        # soft-link the processing directory without job_id.txt, dir_contents.txt and queue.txt
+        # Sentinel-1 has more files starting with d as Doris-v5 output, other sensors do not have that
+        if out_parameters["sensor"] == "S1":
+            link_keys = ["[bgiprs]*", "doris*", "dem"]
+        else:
+            link_keys = ["[bgiprs]*"]
+        for key in link_keys:
+            # run the soft-link command
+            os.system(f"ln -sfn {coregistration_directory}/{key} {crop_directory}")

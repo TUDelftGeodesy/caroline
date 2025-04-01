32c8d09666fc2b62131fbff3808b22d043320a9f
Simon van Diepen <s.vandiepen98@gmail.com>
2025-04-01 13:34:24 +0200

    Remove prototype directory

--- prototype/caroline/caroline_main/caroline_v0.1/files/stack_stitching/MAIN.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-#PBS -q guest -l nodes=n03-{node}:ppn=1
-
-module load matlab
-matlab -nodisplay -nosplash -nodesktop -r "run('{stitch_dir}/{AoI_name}_s1_{asc_dsc}_t{track}/MAIN.m');exit;"
-
--- prototype/caroline/caroline_main/caroline_v1.0/bin/OLD/OLD_fr_set_nodes_available.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-filename, cpath = argv
-
-f = open(f"{cpath}/auxiliary_files/nodes_available.txt", "w")
-f.write("0")
-f.close()

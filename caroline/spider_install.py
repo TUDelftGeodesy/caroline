04997570952ee97248f1efb152eddff917a512f1
Simon van Diepen <s.vandiepen98@gmail.com>
2025-04-01 13:45:10 +0200

    Fix configuration name

--- caroline/spider_install.py
+++ caroline/spider_install.py
@@ -74,11 +74,11 @@ def _install_plugins() -> None:
     # then the github repositories
     for dependency in GITHUB_DEPENDENCIES.keys():
         os.system(f'''echo "{dependency}..."''')
-        if os.path.exists(f"{CONFIG['CAROLINE_PLUGINS_DIRECTORIES']}/{dependency}"):
+        if os.path.exists(f"{CONFIG['CAROLINE_PLUGINS_DIRECTORY']}/{dependency}"):
             # if the path exists, the directory has already been cloned once before. We need to sync with git pull
             # to the right branch. This is one command as we need to change directories
             os.system(
-                f"cd {CONFIG['CAROLINE_PLUGINS_DIRECTORIES']}/{dependency}; git pull; "
+                f"cd {CONFIG['CAROLINE_PLUGINS_DIRECTORY']}/{dependency}; git pull; "
                 f"git checkout {GITHUB_DEPENDENCIES[dependency]['branch']}; git pull"
             )
         else:

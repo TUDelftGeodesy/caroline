4d06a2a320fb52a35428e3ced684dfe42ba75c13
Simon van Diepen <s.vandiepen98@gmail.com>
2025-08-05 10:18:47 +0200

    Fix track readout

--- caroline/kml.py
+++ caroline/kml.py
@@ -553,7 +553,10 @@ def add_coregistered_stack_folder(kml: KML) -> KML:
                     if "doris:general:directory" in param_file_data[param_file_AoI_name].keys():
                         if param_file_data[param_file_AoI_name]["doris:general:directory"] == check_coreg_directory:
                             if param_file_data[param_file_AoI_name]["doris:general:AoI-name"] == AoI_name:
-                                if check_track in param_file_data[param_file_AoI_name]["general:tracks:track-list"]:
+                                if (
+                                    check_track
+                                    in param_file_data[param_file_AoI_name]["general:tracks:track-list-full"]
+                                ):
                                     workflows.append(param_file_AoI_name)
                 if len(workflows) > 0:
                     message += "Part of CAROLINE workflows " + ", ".join(workflows)
